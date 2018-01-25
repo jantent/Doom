4e97292b87c210cb8360130e7aa5c24086b2664d
tangj <840391701@qq.com>
2018-01-25 23:25:29 +0800

    文章列表

--- src/main/java/springBoot/controller/admin/ArticleController.java
+++ src/main/java/springBoot/controller/admin/ArticleController.java
@@ -4,9 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import springBoot.controller.BaseController;
 import springBoot.exception.TipException;
+import springBoot.service.IContentService;
+import springBoot.service.ILogService;
+import springBoot.service.IMetaService;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 文章控制
@@ -20,5 +28,19 @@ import springBoot.exception.TipException;
 public class ArticleController extends BaseController{
     private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
 
+    @Resource
+    private IContentService contentService;
 
+    @Resource
+    private IMetaService metaService;
+
+    @Resource
+    private ILogService logService;
+
+    @GetMapping(value = "")
+    public String index(@RequestParam(value = "page",defaultValue = "1")int page,
+                        @RequestParam(value = "limit",defaultValue = "15")int limit,
+                        HttpServletRequest request){
+        return "admin/article_list";
+    }
 }
--- src/main/java/springBoot/service/impl/ContentServcieImpl.java
+++ src/main/java/springBoot/service/impl/ContentServcieImpl.java
@@ -17,6 +17,7 @@ import springBoot.service.IMetaService;
 import springBoot.service.IRelationshipService;
 import springBoot.util.DateKit;
 import springBoot.util.MyUtils;
+import springBoot.util.Tools;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -108,42 +109,113 @@ public class ContentServcieImpl implements IContentService {
 
     @Override
     public ContentVo getContents(String id) {
-
+        if (StringUtils.isNotBlank(id)) {
+            if (Tools.isNumber(id)) {
+                ContentVo contentVo = contentDao.selectByPrimaryKey(Integer.valueOf(id));
+                if (contentVo != null) {
+                    contentVo.setHits(contentVo.getHits() + 1);
+                    contentDao.updateByPrimaryKey(contentVo);
+                }
+                return contentVo;
+            } else {
+                ContentVoExample contentVoExample = new ContentVoExample();
+                contentVoExample.createCriteria().andSlugEqualTo(id);
+                List<ContentVo> contentVos = contentDao.selectByExampleWithBLOBs(contentVoExample);
+                if (contentVos.size() != 1) {
+                    throw new TipException("query content by id and return is not one");
+                }
+                return contentVos.get(0);
+            }
+        }
         return null;
     }
 
     @Override
     public void updateContentByCid(ContentVo contentVo) {
-
+        if (null != contentVo && null != contentVo.getCid()) {
+            contentDao.updateByPrimaryKeySelective(contentVo);
+        }
     }
 
     @Override
     public PageInfo<ContentVo> getArticles(Integer mid, int page, int limit) {
-        return null;
+        int total = metaDao.countWithSql(mid);
+        PageHelper.startPage(page, limit);
+        List<ContentVo> list = contentDao.findByCatalog(mid);
+        PageInfo<ContentVo> paginator = new PageInfo<>(list);
+        paginator.setTotal(total);
+        return paginator;
     }
 
     @Override
     public PageInfo<ContentVo> getArticles(String keyword, Integer page, Integer limit) {
-        return null;
+        PageHelper.startPage(page, limit);
+        ContentVoExample contentVoExample = new ContentVoExample();
+        ContentVoExample.Criteria criteria = contentVoExample.createCriteria();
+        criteria.andTypeEqualTo(Types.ARTICLE.getType());
+        criteria.andStatusEqualTo(Types.PUBLISH.getType());
+        criteria.andTitleLike("%" + keyword + "%");
+        contentVoExample.setOrderByClause("created desc");
+        List<ContentVo> contentVos = contentDao.selectByExampleWithBLOBs(contentVoExample);
+        return new PageInfo<>(contentVos);
     }
 
     @Override
     public PageInfo<ContentVo> getArticlesWithpage(ContentVoExample commentVoExample, Integer page, Integer limit) {
-        return null;
+        PageHelper.startPage(page, limit);
+        List<ContentVo> contentVos = contentDao.selectByExampleWithBLOBs(commentVoExample);
+        return new PageInfo<>(contentVos);
     }
 
     @Override
     public void deleteByCid(Integer cid) {
-
+        ContentVo contentVo = this.getContents(cid + "");
+        if (null != contentVo) {
+            contentDao.deleteByPrimaryKey(cid);
+            relationshipService.deleteById(cid, null);
+        }
     }
 
     @Override
     public void updateArticle(ContentVo contents) {
+        if (null == contents || null == contents.getCid()) {
+            throw new TipException("文章对象不能为空");
+        }
+        if (StringUtils.isBlank(contents.getTitle())) {
+            throw new TipException("文章标题不能为空");
+        }
+        if (StringUtils.isBlank(contents.getContent())) {
+            throw new TipException("文章内容不能为空");
+        }
+        if (contents.getTitle().length() > 200) {
+            throw new TipException("文章标题过长");
+        }
+        if (contents.getContent().length() > 65000) {
+            throw new TipException("文章内容过长");
+        }
+        if (null == contents.getAuthorId()) {
+            throw new TipException("请登录后发布文章");
+        }
+        if (StringUtils.isBlank(contents.getSlug())) {
+            contents.setSlug(null);
+        }
+        int time = DateKit.getCurrentUnixTime();
+        contents.setModified(time);
+        Integer cid = contents.getCid();
+        contents.setContent(EmojiParser.parseToAliases(contents.getContent()));
 
+        contentDao.updateByPrimaryKeySelective(contents);
+        relationshipService.deleteById(cid, null);
+        metasService.saveMetas(cid, contents.getTags(), Types.TAG.getType());
+        metasService.saveMetas(cid, contents.getCategories(), Types.CATEGORY.getType());
     }
 
     @Override
     public void updateCategory(String ordinal, String newCatefory) {
-
+        ContentVo contentVo = new ContentVo();
+        contentVo.setCategories(newCatefory);
+        ContentVoExample example = new ContentVoExample();
+        example.createCriteria().andCategoriesEqualTo(ordinal);
+        contentDao.updateByExampleSelective(contentVo, example);
     }
 }
